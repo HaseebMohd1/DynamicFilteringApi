375b7ae5acb86d814bf2f36a7ef93214f4d87ade
Haseeb Mohd <haseeb.m@technovert.com>
2024-09-25 19:17:44 +0530

    Add project files.

---
+++ DynamicFilteringApi/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using DynamicFilteringApi.Database;
+using DynamicFilteringApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace DynamicFilteringApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private readonly ProductDbContext productDbContext;
+
+    public ProductController(ProductDbContext productDbContext)
+    {
+        this.productDbContext = productDbContext;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetProducts()
+    {
+        await this.productDbContext.Database.EnsureCreatedAsync();
+        return Ok(productDbContext.Products);
+    }
+
+    [HttpPost("search-product")]
+    public async Task<IActionResult> SearchProducts([FromBody]ProductSearchCriteria productSearch)
+    {
+        await this.productDbContext.Database.EnsureCreatedAsync();
+
+        ParameterExpression parameter = Expression.Parameter(typeof(Product), "x"); // x =>
+        Expression predicate = Expression.Constant(true); // x => true
+
+        if ( productSearch.IsActive.HasValue )
+        {
+            MemberExpression memberExpression = Expression.Property(parameter, nameof(Product.IsActive)); // x.IsActive
+            ConstantExpression constantExpression = Expression.Constant(productSearch.IsActive.Value); // true
+
+            BinaryExpression binaryExpression = Expression.Equal(memberExpression, constantExpression); // x.IsActive == true
+            predicate = Expression.AndAlso(predicate, binaryExpression); // x => true && x.IsActive == true
+        }
+
+        var lambdaExpression = Expression.Lambda<Func<Product, bool>>(predicate, parameter); // x => true && x.IsActive == true
+        var data = await productDbContext.Products.Where(lambdaExpression).ToListAsync();
+
+        return Ok(data);
+    }
+}
---
+++ DynamicFilteringApi/Database/ProductDbContext.cs
@@ -0,0 +1,28 @@
+﻿using DynamicFilteringApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DynamicFilteringApi.Database;
+
+public class ProductDbContext : DbContext
+{ 
+    public DbSet<Product> Products { get; set; }
+
+    public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>().HasData(
+            new Product { Id = 1, Name = "Keyboard", Category = "Electronics", Price = 20.00m, IsActive = true },
+            new Product { Id = 2, Name = "Mouse", Category = "Electronics", Price = 10.00m, IsActive = true },
+            new Product { Id = 3, Name = "Monitor", Category = "Electronics", Price = 100.00m, IsActive = false },
+            new Product { Id = 4, Name = "Headphones", Category = "Electronics", Price = 15.00m, IsActive = true },
+            new Product { Id = 5, Name = "Speakers", Category = "Electronics", Price = 25.00m, IsActive = false },
+            new Product { Id = 6, Name = "Oats", Category = "Food", Price = 500.00m, IsActive = true }
+        );
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
